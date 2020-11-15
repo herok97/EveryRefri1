6cec7c4fb96b5245ef660c179fb2cd84a638aa64
herok97 <herok97@naver.com>
2020-11-15 23:15:17 +0900

    액티비티2번까지 구조만!

--- app/src/main/java/com/example/everyrefri/MainActivity.java
+++ app/src/main/java/com/example/everyrefri/MainActivity.java
@@ -2,14 +2,46 @@ package com.example.everyrefri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // 액티비티 요소들 선언
+    private Button bt_login;
+    private Button bt_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 30;
+
+        // 액티비티 요소들 할당
+        bt_login = findViewById(R.id.bt_sign_up);
+        bt_sign_up = findViewById(R.id.bt_login);
+
+        // 로그인 버튼 이벤트 처리 (onclick이랑 다른 방법이며, 오류가 더 적다고 합니다. 한 덩어리로 생각하시면 좋을 것 같아요)
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // 버튼 클릭시 아래 내용 수행
+                Intent intent = new Intent(getApplicationContext(), activity_2_sign_in.class);
+                startActivityForResult(intent,2);
+            }
+        });
+
+        // 회원가입 버튼 이벤트 처리
+        bt_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // 버튼 클릭시 아래 내용 수행
+                Intent intent = new Intent(getApplicationContext(), activity_3_sign_up.class);
+                startActivityForResult(intent,3);
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/everyrefri/activity_2_sign_in.java
@@ -0,0 +1,46 @@
+package com.example.everyrefri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class activity_2_sign_in extends AppCompatActivity {
+
+    // 액티비티 요소들 선언
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2_sign_in);
+
+        // 액티비티 요소들 할당
+        Button bt_next = findViewById(R.id.bt_next);
+        ImageButton ibt_back = findViewById(R.id.ibt_back);
+        EditText et_id = findViewById(R.id.et_id);
+        EditText et_email = findViewById(R.id.et_email);
+        EditText et_pass = findViewById(R.id.et_pass);
+        EditText et_passconfirm = findViewById(R.id.et_passconfirm);
+
+        // 다음 버튼 이벤트 처리
+        bt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // 버튼 클릭시 아래 내용 수행
+                Intent intent = new Intent(getApplicationContext(), activity_2_sign_in.class);
+                startActivityForResult(intent,2);
+            }
+        });
+
+        // 뒤로가기 버튼 이벤트 처리
+        ibt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // 버튼 클릭시 아래 내용 수행
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+}
