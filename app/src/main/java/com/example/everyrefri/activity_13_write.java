66a881710ba3ff10bdf24bff48e8273fbaf0185b
herok97 <herok97@naver.com>
2020-11-26 11:44:14 +0900

    게시물에 사진 올리는거 일단 구현

--- app/src/main/java/com/example/everyrefri/activity_13_write.java
+++ app/src/main/java/com/example/everyrefri/activity_13_write.java
@@ -1,62 +1,219 @@
 package com.example.everyrefri;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class activity_13_write extends AppCompatActivity {
-    TextView textView;
-    String[] items = {"채소류", "과일류", "인스턴트류", "기타"};
-    String[] methods = {"냉장", "냉동", "실온"};
 
+    private User user;
+    private Spinner sp_category, sp_storage;
+    private Button bt_choice;
+    private ImageButton ibt_save;
+    private ImageView iv_pic;
+    private ArrayAdapter<String> adapter;
+    private String[] items = {"채소류", "과일류", "인스턴트류", "기타"};
+    private String[] methods = {"냉장", "냉동", "실온"};
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_13_write);
 
-        Spinner sp_category = findViewById(R.id.sp_category);
-        Spinner sp_storage = findViewById(R.id.sp_storage);
+        // 객체 할당
+        sp_category = findViewById(R.id.sp_category);
+        sp_storage = findViewById(R.id.sp_storage);
+        bt_choice = findViewById(R.id.bt_choice);
+        ibt_save = findViewById(R.id.ibt_save);
+        iv_pic = findViewById(R.id.iv_pic);
+
+        // 이전 액티비티의 데이터 수신
+        Intent intent =getIntent();
+        user = getUser(intent);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, items
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         sp_category.setAdapter(adapter);
-        sp_category.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                textView.setText(items[position]);
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-                textView.setText("종류");
-            }
-        });
+//        sp_category.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                textView.setText(items[position]);
+//            }
+//
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                textView.setText("종류");
+//            }
+//        });
 
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, methods
         );
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_item);
         sp_storage.setAdapter(adapter1);
-        sp_storage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                textView.setText(methods[position]);
-            }
+//        sp_storage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                textView.setText(methods[position]);
+//            }
+//
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                textView.setText("방법");
+//            }
+//        });
 
-            public void onNothingSelected(AdapterView<?> parent) {
-                textView.setText("방법");
+        // 사진 선택 버튼 클릭시
+        bt_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //이미지를 선택
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
             }
         });
 
+        ibt_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //업로드
+                uploadFile();
 
+                // 게시판으로 돌아가기
+                Intent intent = new Intent(getApplicationContext(), activity_6_board.class);
+                intent = setUser(intent);
+                startActivityForResult(intent,6);//requestcode이게맞는지 다시 확인
+                finish();
+            }
+        });
     }
+
+
+
+
+    //결과 처리
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //request코드가 0이고 OK를 선택했고 data에 뭔가가 들어 있다면
+        if(requestCode == 0 && resultCode == RESULT_OK){
+            filePath = data.getData();
+            Log.d("TAG", "uri:" + String.valueOf(filePath));
+            try {
+                //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                iv_pic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //upload the file
+    private void uploadFile() {
+        //업로드할 파일이 있으면 수행
+        if (filePath != null) {
+            //업로드 진행 Dialog 보이기
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("업로드중...");
+            progressDialog.show();
+
+            //storage
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+
+            //Unique한 파일명을 만들자.  파일명은  현재 날짜 + 시간 + 아이디 + .png 형태로 구성
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMHH_mmss");
+            Date now = new Date();
+            String filename = formatter.format(now) + user.id + ".png";
+
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            StorageReference storageRef = storage.getReferenceFromUrl("gs://database-f0589.appspot.com/images").child(filename);
+            //올라가거라...
+            storageRef.putFile(filePath)
+                    //성공시
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss(); //업로드 진행 Dialog 상자 닫기
+                            Toast.makeText(getApplicationContext(), "업로드 완료!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //실패시
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //진행중
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests") //이걸 넣어 줘야 아랫줄에 에러가 사라진다.
+                                    double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                            //dialog에 진행률을 퍼센트로 출력해 준다
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private User getUser(Intent intent)
+    {
+        User _user = new User(
+                intent.getExtras().getString("id"),
+                intent.getExtras().getString("email"),
+                intent.getExtras().getInt("follower"),
+                intent.getExtras().getInt("following"),
+                intent.getExtras().getFloat("grade"));
+        return _user;
+    }
+
+    private Intent setUser(Intent intent)
+    {
+        intent.putExtra("id", user.id);
+        intent.putExtra("follower", user.follower);
+        intent.putExtra("following", user.following);
+        intent.putExtra("grade", user.grade);
+        intent.putExtra("email", user.email);
+        return intent;
+    }
+
+
 }
