6995c75e9ccad90fa862f6107811cec2ba18d470
Yulim KIM <eulim99226@naver.com>
2020-12-13 00:31:03 +0900

    search location-> latitude,longitude

--- app/src/main/java/com/example/everyrefri/activity_11_location.java
+++ app/src/main/java/com/example/everyrefri/activity_11_location.java
@@ -3,17 +3,27 @@ package com.example.everyrefri;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationProvider;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.LocationTrackingMode;
@@ -23,118 +33,116 @@ import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.internal.HTTPRequest;
+import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.util.MarkerIcons;
+
+import java.io.IOException;
+import java.util.List;
 
 public class activity_11_location extends AppCompatActivity implements OnMapReadyCallback{
+
+    private MapView mapView;
+    private EditText txtsearch;
+    private Button bt_search;
+    private Button save;
+    private ImageButton ibt_back;
+    private TextView locationText;
+    private User user;
+    private FirebaseAuth fireAuth;
+    private FirebaseStorage storage;
+    private Uri filePath;
+    private NaverMap naverMap=null;
+    private CameraUpdate cameraUpdate=null;
+
+
+
     @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-
-    }
-
-    // static final String TAG="LOCATION";
-    //MapFragment mapFragment;
-    //NaverMap map;
-
-    //위치 검색해서 지도에서 넣는건 쉽지 않아보여서
-    // 현재 위치 파악으로 버튼눌러 그위치를 저장하는 식으로 하려고하는 중인데 원래나오던 지도도 안나오는중..
-    //오류 더 찾고 확인해야함
-
-    public class MapApi extends AppCompatActivity implements OnMapReadyCallback {
-
-        private MapView mapView;
-
-        private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
-        private FusedLocationSource locationSource;
-        private NaverMap naverMap;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_11_location);
-
-
-            mapView = findViewById(R.id.mapView);
-            mapView.onCreate(savedInstanceState);
-            mapView.getMapAsync(this);
-
-            locationSource =
-                    new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
-
-
-
-        }
-        //위치정보 권한 설정
-        @Override
-        public void onRequestPermissionsResult(int requestCode,
-                                               @NonNull String[] permissions,  @NonNull int[] grantResults) {
-            if (locationSource.onRequestPermissionsResult(
-                    requestCode, permissions, grantResults)) {
-                return;
-            }
-            super.onRequestPermissionsResult(
-                    requestCode, permissions, grantResults);
-        }
-
-        @Override
-        public void onMapReady(@NonNull NaverMap naverMap) {
-            this.naverMap = naverMap;
-            // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
-            naverMap.setLocationSource(locationSource);
-            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-
-            UiSettings uiSettings = naverMap.getUiSettings();
-            uiSettings.setCompassEnabled(true); // 나침반
-            uiSettings.setScaleBarEnabled(true); // 거리
-            uiSettings.setZoomControlEnabled(true); // 줌
-            uiSettings.setLocationButtonEnabled(true); // 내가 있는곳
-
-
-        }
-    }
-
-
-    /*protected void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_11_location);
 
-       mapFragment=(MapFragment)getSupportFragmentManager().findFragmentById(R.id.mapView);
-        //mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync(new OnMapReadyCallback(){
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+
+        storage = FirebaseStorage.getInstance();
+        Intent intent =getIntent();
+        user = new User();
+       // user.getUserFromIntent(intent);
+
+        bt_search= findViewById(R.id.bt_search);
+        txtsearch=findViewById(R.id.et_location);
+        locationText=findViewById(R.id.tv_view);
+        ibt_back=findViewById(R.id.ibt_back11);
+
+        bt_search.setOnClickListener(new View.OnClickListener(){
+
+
             @Override
-            public void onMapReady(@NonNull NaverMap naverMap) {
-                Log.d(TAG,"Naver MAP is ready");
-                map=naverMap;
+            public void onClick(View v) {
+                String  txt = txtsearch.getText().toString();
+                Geocoder geocoder = new Geocoder(getBaseContext());
+                List<Address> addresses = null;
+
+                try{
+                    addresses= geocoder.getFromLocationName(txt,3);
+                    if(addresses!=null&&!addresses.equals("")){
+                        search(addresses);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
-        try{
-            MapsInitializer.initialize(this);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        Button button = (Button)findViewById(R.id.bt_locationsave);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                new LocationRequest();
+
+
+
+        ibt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity_7_myprofile.class);
+                intent = user.setUserToIntent(intent);
+                startActivityForResult(intent,7);
             }
-        });*/
+        });
+
+
+
+        }
+
+    protected void search(List<Address> addresses) {
+        Address address= addresses.get(0);
+        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+
+        String addressText=String.format(
+                "%s, %s",
+                address.getMaxAddressLineIndex()>0 ? address.getAddressLine(0):" ",address.getFeatureName()
+        );
+        locationText.setVisibility(View.VISIBLE);
+        locationText.setText("Latitude"+address.getLatitude()+"Longitude"+address.getLongitude()+"\n"+addressText);
+        Marker marker=new Marker();
+        marker.setPosition(new LatLng(address.getLatitude(),address.getLongitude()));
+        //NaverMap naverMap;
+        //marker.setMap(naverMap);
+        //cameraUpdate = CameraUpdate.scrollTo(new LatLng(address.getLatitude(),address.getLongitude()));
+        //naverMap.moveCamera(cameraUpdate);
 
     }
 
-    /*public void onLocationChanged(Location location){
-
-    }
-
-
-    private void showCurrentLocation(double latitude,double longitude){
-        LatLng curPoint = new LatLng(latitude,longitude);
-        map.moveCamera(CameraUpdate.scrollAndZoomTo(curPoint,15));
-
-    }*/
-
-/*
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-
+        //Marker marker = new Marker();
+        //LatLng loc=new LatLng(address.getLatitude);
+        //marker.setPosition(loc);
+        //marker.setMap(naverMap);
+        //naverMap.moveCamera(CameraUpdate.scrollTo(loc));
     }
-}*/
+
+
+
+
+}
+
